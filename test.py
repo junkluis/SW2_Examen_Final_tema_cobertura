afe1d88c0ad2a303e16f8ebeef98871e80291950
luis zuniga <lufezuro@gmail.com>
2019-08-05 01:39:44 -0500

    testing

--- test.py
+++ test.py
@@ -12,7 +12,11 @@ class Test(unittest.TestCase):
 	def test_nombre_casos_prueba(self):
 		self.assertEqual('','')
 
-	
+	def test_calcular_descuento_cliente_jovenes(self):
+		mensaje, descuento = descuentos.calcular_descuento(19,502,0)
+		self.assertEqual(mensaje, 'Descuento clientes iniciales')
+		self.assertEqual(descuento, 100.4)
+
 
 
 
