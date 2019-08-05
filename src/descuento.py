9ed17c4f24bff24c2c3f8b91f8fc22dddca889de
luis zuniga <lufezuro@gmail.com>
2019-08-05 02:10:41 -0500

    testing

--- src/descuento.py
+++ src/descuento.py
@@ -8,23 +8,23 @@ def calcular_descuento(edad, valor_cotizado, dependientes):
     #Calcular el descuento considerando la edad, numero de dependientes y valor cotizado
     mensaje = ""
     descuento = 0
-    EDAD = edad
-    if(edad > 18 and valor_cotizado > 500 and dependientes >=0 ):
+    
+    if(edad > 18 and valor_cotizado > 500 and dependientes >= 0 ):
         
-        if((18<edad<27) or (750<valor_cotizado<1000) ):
+        if((18 < edad < 27) or (750 < valor_cotizado < 1000) ):
             mensaje = "Descuento clientes iniciales"
             descuento = valor_cotizado*(0.2)
 
-        elif( (valor_cotizado>1000) and (27<edad<=65)):
+        elif( (valor_cotizado > 1000) and (27 < edad <= 65)):
 
-            if( (35<=edad<50 and dependientes>0) or (dependientes>=4)  ):
+            if( (35 <= edad < 50 and dependientes > 0) or (dependientes >= 4)  ):
                 mensaje = "Descuento para familias"
-                if (dependientes>=6):
+                if (dependientes >= 6):
                     descuento = valor_cotizado*(0.1*dependientes)
                 else:
                     descuento = valor_cotizado*(0.05*dependientes)
 
-            elif( 50<=edad<=65):
+            elif( 50 <= edad <= 65):
                 mensaje = "Descuento especiales"
                 descuento = valor_cotizado*(0.35)
 
@@ -32,9 +32,9 @@ def calcular_descuento(edad, valor_cotizado, dependientes):
                 mensaje = "No aplica"
                 descuento = 0                
 
-        elif( (edad>=80) or (edad>65 and dependientes==0) ):
+        elif( (edad >= 80) or (edad > 65 and dependientes == 0) ):
             mensaje = "Descuento para mayores de edad"
-            if(valor_cotizado >2000):
+            if(valor_cotizado > 2000):
                 descuento = valor_cotizado*(0.5)
             else:
                 descuento = valor_cotizado*(0.25)
