061d33951a9e03b6041c43ea5fd3e2a420e8f157
kbburgos@espol.edu.ec <kbburgos@espol.edu.ec>
2019-08-26 08:34:13 -0500

    Fina_tema_cobertura

--- test.py
+++ test.py
@@ -11,9 +11,104 @@ import src.descuento as descuentos
 class TestDescuentos(unittest.TestCase):
     ''' Pruebas para calculo del descuento para clientes '''
 
-    def test_nombre_casos_prueba(self):
+    def test_mayor_edad(self):
         ''' Caso de prueba '''
-        self.assertEqual('', '')
+        edad = 19
+        valor_cotizado = 501
+        dependientes = 0
+        mjs_esperado = ("Descuento clientes iniciales", 100.2)
+        mjs = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mjs, mjs_esperado)
+
+    def test_mayor_dependiente_7(self):
+        ''' Caso de prueba '''
+        edad = 35
+        valor_cotizado = 1001
+        dependientes = 7
+        mjs_esperado = ("Descuento para familias", 700.7)
+        mjs = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mjs, mjs_esperado)
+
+    def test_mayor_edad_2(self):
+        ''' Caso de prueba '''
+        edad = 51
+        valor_cotizado = 1001
+        dependientes = 7
+        mjs_esperado = ("Descuento para familias", 700.7)
+        mjs = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mjs, mjs_esperado)
+
+    def test_mayor_dependiente_5(self):
+        ''' Caso de prueba '''
+        edad = 51
+        valor_cotizado = 1001
+        dependientes = 5
+        mjs_esperado = ("Descuento para familias", 250.25)
+        mjs = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mjs, mjs_esperado)
+
+    def test_mayor_dependiente_3(self):
+        ''' Caso de prueba '''
+        edad = 51
+        valor_cotizado = 1001
+        dependientes = 3
+        mjs_esperado = ('Descuento especiales', 350.34999999999997)
+        mjs = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mjs, mjs_esperado)
+
+    def test_mayor_dependiente_0(self):
+        ''' Caso de prueba '''
+        edad = 49
+        valor_cotizado = 1001
+        dependientes = 0
+        mjs_esperado = ('No aplica', 0)
+        mjs = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mjs, mjs_esperado)
+
+    def test_edad_80(self):
+        ''' Caso de prueba '''
+        edad = 80
+        valor_cotizado = 2001
+        dependientes = 0
+        mjs_esperado = ("Descuento para mayores de edad", 1000.5)
+        mjs = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mjs, mjs_esperado)
+
+    def test_edad_80_dependiente_0(self):
+        ''' Caso de prueba '''
+        edad = 80
+        valor_cotizado = 1001
+        dependientes = 0
+        mjs_esperado = ("Descuento para mayores de edad", 250.25)
+        mjs = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mjs, mjs_esperado)
+
+    def test_edad_69_dependiente_1(self):
+        ''' Caso de prueba '''
+        edad = 69
+        valor_cotizado = 5001
+        dependientes = 1
+        mjs_esperado = ("Descuento para clientes", 100.02)
+        mjs = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mjs, mjs_esperado)
+
+    def test_edad_69_dependiente_1_valor_4000(self):
+        ''' Caso de prueba '''
+        edad = 69
+        valor_cotizado = 4001
+        dependientes = 1
+        mjs_esperado = ("Descuento para clientes", 40.01)
+        mjs = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mjs, mjs_esperado)
+
+    def test_menor_edad(self):
+        ''' Caso de prueba '''
+        edad = 17
+        valor_cotizado = 4001
+        dependientes = 1
+        mjs_esperado = ("Descuento no valido", 0)
+        mjs = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mjs, mjs_esperado)
 
 
 
