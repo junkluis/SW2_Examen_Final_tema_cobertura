12b2a5413e9a13bb885fff733472d80ec3477ac8
Byron Povea <bpovea@espol.edu.ec>
2019-08-26 08:36:59 -0500

    [ADD] coverage tests

--- test.py
+++ test.py
@@ -7,14 +7,92 @@ import xmlrunner
 import src.descuento as descuentos
 
 
-
 class TestDescuentos(unittest.TestCase):
     ''' Pruebas para calculo del descuento para clientes '''
 
-    def test_nombre_casos_prueba(self):
+    def test_menor_edad(self):
         ''' Caso de prueba '''
-        self.assertEqual('', '')
+        edad, valor_cotizado, dependientes = 10, 500, 1
+        mensaje, descuento = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mensaje, 'Descuento no valido')
+        self.assertEqual(descuento, 0)
 
+    def test_menos_500(self):
+        ''' Caso de prueba '''
+        edad, valor_cotizado, dependientes = 19, 400, 1
+        mensaje, descuento = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mensaje, 'Descuento no valido')
+        self.assertEqual(descuento, 0)
+
+    def test_dependientes_negativos(self):
+        ''' Caso de prueba '''
+        edad, valor_cotizado, dependientes = 19, 600, -1
+        mensaje, descuento = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mensaje, 'Descuento no valido')
+        self.assertEqual(descuento, 0)
+
+    def test_clientes_iniciales(self):
+        ''' Caso de prueba '''
+        edad, valor_cotizado, dependientes = 19, 600, 1
+        mensaje, descuento = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mensaje, 'Descuento clientes iniciales')
+        self.assertEqual(descuento, valor_cotizado*0.2)
+
+    def test_familias_1(self):
+        ''' Caso de prueba '''
+        edad, valor_cotizado, dependientes = 35, 1600, 1
+        mensaje, descuento = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mensaje, 'Descuento para familias')
+        self.assertEqual(descuento, valor_cotizado*(0.05*dependientes))
+
+    def test_familias_2(self):
+        ''' Caso de prueba '''
+        edad, valor_cotizado, dependientes = 28, 1600, 6
+        mensaje, descuento = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mensaje, 'Descuento para familias')
+        self.assertEqual(descuento, valor_cotizado*(0.1*dependientes))
+
+    def test_especiales(self):
+        ''' Caso de prueba '''
+        edad, valor_cotizado, dependientes = 51, 1600, 1
+        mensaje, descuento = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mensaje, 'Descuento especiales')
+        self.assertEqual(descuento, valor_cotizado*0.35)
+
+    def test_no_aplica(self):
+        ''' Caso de prueba '''
+        edad, valor_cotizado, dependientes = 28, 1600, 0
+        mensaje, descuento = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mensaje, 'No aplica')
+        self.assertEqual(descuento, 0)
+
+    def test_mayor_edad_1(self):
+        ''' Caso de prueba '''
+        edad, valor_cotizado, dependientes = 82, 1600, 0
+        mensaje, descuento = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mensaje, 'Descuento para mayores de edad')
+        self.assertEqual(descuento, valor_cotizado*(0.25))
+
+    def test_mayor_edad_2(self):
+        ''' Caso de prueba '''
+        edad, valor_cotizado, dependientes = 70, 2500, 0
+        mensaje, descuento = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mensaje, 'Descuento para mayores de edad')
+        self.assertEqual(descuento, valor_cotizado*0.5)
+
+    def test_clientes_1(self):
+        ''' Caso de prueba '''
+        edad, valor_cotizado, dependientes = 27, 6000, 0
+        mensaje, descuento = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mensaje, 'Descuento para clientes')
+        self.assertEqual(descuento, valor_cotizado*0.02)
+
+    def test_clientes_2(self):
+        ''' Caso de prueba '''
+        edad, valor_cotizado, dependientes = 27, 3000, 0
+        mensaje, descuento = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(mensaje, 'Descuento para clientes')
+        self.assertEqual(descuento, valor_cotizado*0.01)
 
 
 if __name__ == '__main__':
