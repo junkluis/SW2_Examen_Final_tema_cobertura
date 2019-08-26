50305063d02668d0b1ab89ddce812975a95fe4fa
dfsaigua <dfsaigua@espol.edu.ec>
2019-08-26 10:05:05 -0500

    pruebas3 y 4

--- test.py
+++ test.py
@@ -16,11 +16,20 @@ class TestDescuentos(unittest.TestCase):
         msj_esperado = 'Descuento clientes iniciales'
         self.assertEqual(msj_esperado, mensaje)
 
-    def test_comprobar_variables(self): 
+    def test_comprobar_variables2(self): 
         mensaje, descuento = calcular_descuento(16,400,0)
         msj_esperado = 'Descuento no valido'
         self.assertEqual(msj_esperado, mensaje)
 
+    def test_comprobar_variables3(self): 
+        mensaje, descuento = calcular_descuento(36,2,5)
+        msj_esperado = 'Descuento no valido'
+        self.assertEqual(msj_esperado, mensaje)
+
+    def test_comprobar_variables4(self): 
+        mensaje, descuento = calcular_descuento(52,2,5)
+        msj_esperado = 'Descuento no valido'
+        self.assertEqual(msj_esperado, mensaje)
 
 if __name__ == '__main__':
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='reporte-pruebas'))
