f3f1f8f2a4f703257424f33c08bd27650c66b585
dfsaigua <dfsaigua@espol.edu.ec>
2019-08-26 09:46:42 -0500

    prueba1

--- test.py
+++ test.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
 """Archivo de pruebas."""
 from __future__ import unicode_literals
-
 import unittest
 import xmlrunner
-import src.descuento as descuentos
-
+#import src.descuento as descuentos
+from src.descuento import *
 
 
 class TestDescuentos(unittest.TestCase):
     ''' Pruebas para calculo del descuento para clientes '''
+    
 
-    def test_nombre_casos_prueba(self):
-        ''' Caso de prueba '''
-        self.assertEqual('', '')
+    def test_comprobar_variables(self): 
+        mensaje, descuento = calcular_descuento(19,501,0)
+        msj_esperado = 'Descuento clientes iniciales'
+        self.assertEqual(msj_esperado, mensaje)
 
 
 
 if __name__ == '__main__':
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='reporte-pruebas'))
+
+
