99e57452ad4e237aabca5c482365fb456e584514
llullanqui <luis.1142@hotmail.com>
2019-08-26 08:26:31 -0500

    pylint descuento.py

--- test.py
+++ test.py
@@ -11,11 +11,29 @@ import src.descuento as descuentos
 class TestDescuentos(unittest.TestCase):
     ''' Pruebas para calculo del descuento para clientes '''
 
-    def test_nombre_casos_prueba(self):
-        ''' Caso de prueba '''
-        self.assertEqual('', '')
+    def prueba_edad_invalida(self):
+        '''Prueba en caso de edad inválida'''
+        edad = 15
+        valor_cotizado = 600
+        dependientes = 0
+        retorno = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(("Descuento no valido", 0), retorno)
 
+    def prueba_valor_invalido(self):
+        '''Prueba en caso de valor inválido'''
+        edad = 19
+        valor_cotizado = 400
+        dependientes = 0
+        retorno = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(("Descuento no valido", 0), retorno)
 
+    def pruena_dependientes_invalidos(self):
+        '''Prueba en caso de dependientes inválidos'''
+        edad = 19
+        valor_cotizado = 600
+        dependientes = -1
+        retorno = descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        self.assertEqual(("Descuento no valido", 0), retorno)
 
 if __name__ == '__main__':
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='reporte-pruebas'))
