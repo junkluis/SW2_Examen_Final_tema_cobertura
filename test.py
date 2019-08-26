cc65124aaa60d81cfe45ddc319a42d384857fd45
rbloor97 <rbloor97@hotmail.com>
2019-08-26 08:34:33 -0500

    Iniciando pruebas de cobertura de codigo

--- test.py
+++ test.py
@@ -15,6 +15,37 @@ class TestDescuentos(unittest.TestCase):
         ''' Caso de prueba '''
         self.assertEqual('', '')
 
+    def test_calcular_descuento_clientes_iniciales(self):
+        msj_esperado = "Descuento clientes iniciales"
+        valor_cotizado = 751
+        descuento_esperado = valor_cotizado*0.2
+        msj, descuento = calcular_descuento(19, valor_cotizado, 0)
+
+        self.assertEqual(msj, msj_esperado)
+        self.assertEqual(descuento, descuento_esperado)
+
+    def test_calcular_descuento_familias(self):
+        msj_esperado = "Descuento para familias"
+        valor_cotizado = 1001
+        dependientes = 6
+        descuento_esperado = valor_cotizado*(0.1*dependientes)
+        msj, descuento = calcular_descuento(19, valor_cotizado, dependientes)
+
+        self.assertEqual(msj, msj_esperado)
+        self.assertEqual(descuento, descuento_esperado)
+
+    def test_calcular_descuento_familias_pequenias(self):
+        msj_esperado = "Descuento para familias"
+        valor_cotizado = 1001
+        dependientes = 5
+        descuento_esperado = valor_cotizado*(0.05*dependientes)
+        msj, descuento = calcular_descuento(19, valor_cotizado, dependientes)
+
+        self.assertEqual(msj, msj_esperado)
+        self.assertEqual(descuento, descuento_esperado)
+
+
+
 
 
 if __name__ == '__main__':
