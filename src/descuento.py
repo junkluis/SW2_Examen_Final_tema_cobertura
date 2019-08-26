93775241064cfaa9c9b3147e02165697598fc9c8
mangelrivasm <mangelrivasm@hotmail.com>
2019-08-26 09:04:18 -0500

    pylint

--- src/descuento.py
+++ src/descuento.py
@@ -8,7 +8,6 @@ def calcular_descuento(edad, valor_cotizado, dependientes):
     #Calcular el descuento considerando la edad, numero de dependientes y valor cotizado
     mensaje = ""
     descuento = 0
-    EDAD = edad
     if(edad > 18 and valor_cotizado > 500 and dependientes >= 0):
         
         if((18 < edad < 27) or (750 < valor_cotizado < 1000)):
@@ -16,10 +15,8 @@ def calcular_descuento(edad, valor_cotizado, dependientes):
             descuento = valor_cotizado*(0.2)
 
         elif((valor_cotizado > 1000) and (27 < edad <= 65)):
-
-            #pruebas
-
-            if((35 <= edad < 50 and dependientes > 0) or (dependientes >=4 )):
+            
+            if((35 <= edad < 50 and dependientes > 0) or (dependientes >= 4)):
                 mensaje = "Descuento para familias"
                 if (dependientes>=6):
                     descuento = valor_cotizado*(0.1*dependientes)
@@ -32,7 +29,7 @@ def calcular_descuento(edad, valor_cotizado, dependientes):
 
             else:
                 mensaje = "No aplica"
-                descuento = 0               
+                descuento = 0
 
         elif((edad >= 80) or (edad > 65 and dependientes == 0)):
             mensaje = "Descuento para mayores de edad"
@@ -47,13 +44,8 @@ def calcular_descuento(edad, valor_cotizado, dependientes):
                 descuento = valor_cotizado*(0.02)
             else:
                 descuento = valor_cotizado*(0.01)
-            
-
     else:
         mensaje = "Descuento no valido"
         descuento = 0
 
     return mensaje, descuento
-
-
-
