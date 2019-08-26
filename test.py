0593ef56d198483d3663b2b128d392427a152c53
leandre96 <leo_silver12@hotmail.com>
2019-08-26 10:15:54 -0500

    addinf files

--- test.py
+++ test.py
@@ -15,7 +15,41 @@ class TestDescuentos(unittest.TestCase):
         ''' Caso de prueba '''
         self.assertEqual('', '')
 
+    def descuento_no_valido(self):
+        mensaje,descuento=descuentos.calcular_descuento(5, 100, 2)
+        self.assertEqual('Descuento no valido',mensaje)
+        
+    def descuento_cliente_inicial(self):
+        mensaje,descuento=descuentos.calcular_descuento(20, 800, 2)
+        self.assertEqual("Descuento clientes iniciales",mensaje)
 
+    def descuento_familia_pequena(self):
+        mensaje,descuento=descuentos.calcular_descuento(45, 1100, 4)
+        self.assertEqual(220,descuento)
+
+    def descuento_familia_grande(self):
+        mensaje,descuento=descuentos.calcular_descuento(45, 1100, 6)
+        self.assertEqual(660,descuento)
+        
+    def descuento_especial(self):
+        mensaje,descuento=descuentos.calcular_descuento(60, 1000, 6)
+        self.assertEqual(350,descuento)
+        
+    def descuento_mayores_cotizado_bajo(self):
+        mensaje,descuento=descuentos.calcular_descuento(85, 1000, 6)
+        self.assertEqual(250,descuento)
+
+    def descuento_mayores_cotizado_alto(self):
+        mensaje,descuento=descuentos.calcular_descuento(85, 2500, 6)
+        self.assertEqual(1250,descuento)
+
+    def descuento_cliente_cotizado_bajo(self):
+        mensaje,descuento=descuentos.calcular_descuento(70, 1000, 6)
+        self.assertEqual(10,descuento)
+
+    def descuento_cliente_cotizado_alto(self):
+        mensaje,descuento=descuentos.calcular_descuento(70, 6000, 6)
+        self.assertEqual(120,descuento)
 
 if __name__ == '__main__':
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='reporte-pruebas'))
