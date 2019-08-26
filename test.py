dc8ebdee427512f013265454dc56422b5b12c330
unknown <jlmasson@espol.edu.ec>
2019-08-26 08:59:06 -0500

    TEST 5

--- test.py
+++ test.py
@@ -27,5 +27,23 @@ class TestDescuentos(unittest.TestCase):
         self.assertEqual('Descuento clientes iniciales', mensaje)
         self.assertEqual(1000, descuento)
 
+    def test_descuento_familias_no_aplica(self):
+        '''Caso 3: Descuento familias (no aplica)'''
+        mensaje, descuento = descuentos.calcular_descuento(28, 5000, 1)
+        self.assertEqual('No aplica', mensaje)
+        self.assertEqual(0, descuento)
+
+    def test_descuento_familias(self):
+        '''Caso 4: Descuento familias'''
+        mensaje, descuento = descuentos.calcular_descuento(35, 5000, 1)
+        self.assertEqual('Descuento para familias', mensaje)
+        self.assertEqual(250, descuento)
+    
+    def test_descuento_familias_mayor_6(self):
+        '''Caso 5: Descuento familias mayor a 6 dependientes'''
+        mensaje, descuento = descuentos.calcular_descuento(35, 5000, 6)
+        self.assertEqual('Descuento para familias', mensaje)
+        self.assertEqual(3000.0000000000005, descuento)
+
 if __name__ == '__main__':
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='reporte-pruebas'))
