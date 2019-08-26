12e449c072c6ecedba88e85bf3df058742fd63f0
Cliente CONTROL-PC <CltControl@GGIECSOFWRK155.espol.edu.ec>
2019-08-26 08:46:59 -0500

    prueba1

--- test.py
+++ test.py
@@ -6,14 +6,20 @@ import unittest
 import xmlrunner
 import src.descuento as descuentos
 
-
+'''
+caso 1, entre 18 y 27 mayor a 500
+'''
 
 class TestDescuentos(unittest.TestCase):
     ''' Pruebas para calculo del descuento para clientes '''
 
-    def test_nombre_casos_prueba(self):
-        ''' Caso de prueba '''
-        self.assertEqual('',  '')
+    def test_descuento1(self):
+        edad =20
+        valor_cotizado = 600
+        dependientes = 0
+        mensaje_obtenido, descuento_obtenido=calcular_descuento(edad, valor_cotizado, dependientes)
+        mensaje_esperado= "Descuento clientes iniciales"
+        self.assertEqual(mensaje_obtenido, mensaje_esperado)
 
 
 
