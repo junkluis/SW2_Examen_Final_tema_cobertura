bef1c4af0ab3e8f22aebc32837a3fb2630dd2a31
ceva94 <ceva94@hotmail.com>
2019-08-26 08:31:38 -0500

    prueba

--- test.py
+++ test.py
@@ -15,6 +15,13 @@ class TestDescuentos(unittest.TestCase):
         ''' Caso de prueba '''
         self.assertEqual('', '')
 
+    def descuento_inicial(self):
+        mensajeesperado = "Descuento clientes iniciales"
+        descuentoesperado = 800*0.2
+        mensaje, descuento = descuentos.calcular_descuento(20,800,2)
+        self.assertEqual(mensajeesperado, mensaje)
+        self.assertEqual(descuentoesperado, descuento)
+
 
 
 if __name__ == '__main__':
