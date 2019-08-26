6d784469c4d50667ddcd47b17659481719bae35b
AnibalG <aagamboa@espol.edu.ec>
2019-08-26 09:37:50 -0500

    pylint

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ class TestDescuentos(unittest.TestCase):
     ''' Pruebas para calculo del descuento para clientes '''
 
     def test_descuento_clientes_principales(self):
+        ''' Prueba calculo del descuento para clientes principales '''
         edad = 19
         valor = 800
         dependientes = 0
@@ -19,6 +20,7 @@ class TestDescuentos(unittest.TestCase):
         self.assertEqual(result[1], valor*0.2)
 
     def test_descuento_no_valido(self):
+        ''' Prueba calculo del descuento no valido '''
         edad = 12
         valor = 800
         dependientes = 0
@@ -27,6 +29,7 @@ class TestDescuentos(unittest.TestCase):
         self.assertEqual(result[1], 0)
 
     def test_descuento_famiias_menora6(self):
+        ''' Prueba calculo del descuento para familias menor a 6 '''
         edad = 36
         valor = 1200
         dependientes = 4
@@ -35,6 +38,7 @@ class TestDescuentos(unittest.TestCase):
         self.assertEqual(result[1], valor*0.05*dependientes)
 
     def test_descuento_famiias_mayora6(self):
+        ''' Prueba calculo del descuento para familias mayor a 6 '''
         edad = 36
         valor = 1200
         dependientes = 7
@@ -43,6 +47,7 @@ class TestDescuentos(unittest.TestCase):
         self.assertAlmostEqual(result[1], valor*0.1*dependientes)
 
     def test_descuento_especiales(self):
+        ''' Prueba calculo del descuento especial '''
         edad = 55
         valor = 1200
         dependientes = 0
@@ -51,6 +56,7 @@ class TestDescuentos(unittest.TestCase):
         self.assertAlmostEqual(result[1], valor*0.35)
 
     def test_descuento_no_aplica(self):
+        ''' Prueba calculo del descuento no aplica'''
         edad = 28
         valor = 1200
         dependientes = 3
@@ -59,6 +65,7 @@ class TestDescuentos(unittest.TestCase):
         self.assertEqual(result[1], 0)
 
     def test_descuento_mayores_mayora2000(self):
+        ''' Prueba calculo del descuento para mayores de edad '''
         edad = 80
         valor = 3000
         dependientes = 3
@@ -67,6 +74,8 @@ class TestDescuentos(unittest.TestCase):
         self.assertAlmostEqual(result[1], valor*0.5)
 
     def test_descuento_mayores_menor2000(self):
+        ''' Prueba calculo del descuento para mayores de edad con valor menor
+        a 2000'''
         edad = 80
         valor = 1500
         dependientes = 3
@@ -75,6 +84,8 @@ class TestDescuentos(unittest.TestCase):
         self.assertEqual(result[1], valor*0.25)
 
     def test_descuento_clientes_mayor5000(self):
+        ''' Prueba calculo del descuento
+        para clientes con valor mayor a 5000 '''
         edad = 70
         valor = 6000
         dependientes = 3
@@ -83,6 +94,7 @@ class TestDescuentos(unittest.TestCase):
         self.assertEqual(result[1], valor*0.02)
 
     def test_descuento_clientes_menor5000(self):
+        ''' Prueba calculo del descuento para clientes valor mayor 5000 '''
         edad = 70
         valor = 3000
         dependientes = 3
