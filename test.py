95d2d1edb383f2082e52d9ba01d9611300855162
Cliente CONTROL-PC <CltControl@GGIECSOFWRK155.espol.edu.ec>
2019-08-26 09:01:06 -0500

    parte1, prueba 2

--- test.py
+++ test.py
@@ -17,11 +17,65 @@ class TestDescuentos(unittest.TestCase):
         edad =20
         valor_cotizado = 600
         dependientes = 0
-        mensaje_obtenido, descuento_obtenido=calcular_descuento(edad, valor_cotizado, dependientes)
+        mensaje_obtenido, descuento_obtenido=descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
         mensaje_esperado= "Descuento clientes iniciales"
         self.assertEqual(mensaje_obtenido, mensaje_esperado)
 
+    def test_descuento2(self):
+        edad =35
+        valor_cotizado = 800
+        dependientes = 0
+        mensaje_obtenido, descuento_obtenido=descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        mensaje_esperado= "Descuento clientes iniciales"
+        self.assertEqual(mensaje_obtenido, mensaje_esperado)
 
+    def test_descuento_familias1(self):
+        edad =35
+        valor_cotizado = 1200
+        dependientes = 1
+        mensaje_obtenido, descuento_obtenido=descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        mensaje_esperado= "Descuento para familias"
+        self.assertEqual(mensaje_obtenido, mensaje_esperado)
+
+    def test_descuento_familias2(self):
+        edad =35
+        valor_cotizado = 1200
+        dependientes = 5
+        mensaje_obtenido, descuento_obtenido=descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        mensaje_esperado= "Descuento para familias"
+        self.assertEqual(mensaje_obtenido, mensaje_esperado)
+
+    def test_descuento_especiales(self):
+        edad =50
+        valor_cotizado = 1200
+        dependientes = 1
+        mensaje_obtenido, descuento_obtenido=descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        mensaje_esperado= "Descuento especiales"
+        self.assertEqual(mensaje_obtenido, mensaje_esperado)
+
+    def test_descuento_no_aplica(self):
+        edad =45
+        valor_cotizado = 1200
+        dependientes = 0
+        mensaje_obtenido, descuento_obtenido=descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        mensaje_esperado= "No aplica"
+        self.assertEqual(mensaje_obtenido, mensaje_esperado)
+
+    def test_descuento_no_valido1(self):
+        edad =18
+        valor_cotizado = 600
+        dependientes = 0
+        mensaje_obtenido, descuento_obtenido=descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        mensaje_esperado= "Descuento no valido"
+        self.assertEqual(mensaje_obtenido, mensaje_esperado)
+
+    def test_descuento_no_valido2(self):
+        edad =35
+        valor_cotizado = 400
+        dependientes = 0
+        mensaje_obtenido, descuento_obtenido=descuentos.calcular_descuento(edad, valor_cotizado, dependientes)
+        mensaje_esperado= "Descuento no valido"
+        self.assertEqual(mensaje_obtenido, mensaje_esperado)
 
 if __name__ == '__main__':
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='reporte-pruebas'))
