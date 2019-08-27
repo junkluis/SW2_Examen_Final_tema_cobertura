228d47e84ffee21a65e993e223a13c82f699b333
Luis Zuñiga <lufezuro@gmail.com>
2019-08-27 01:12:47 -0500

    Update test.py
    se agrego una modificación para hacer valido el código del estudiante

--- test.py
+++ test.py
@@ -15,39 +15,39 @@ class TestDescuentos(unittest.TestCase):
         ''' Caso de prueba '''
         self.assertEqual('', '')
 
-    def descuento_no_valido(self):
+    def test_descuento_no_valido(self):
         mensaje,descuento=descuentos.calcular_descuento(5, 100, 2)
         self.assertEqual('Descuento no valido',mensaje)
         
-    def descuento_cliente_inicial(self):
+    def test_descuento_cliente_inicial(self):
         mensaje,descuento=descuentos.calcular_descuento(20, 800, 2)
         self.assertEqual("Descuento clientes iniciales",mensaje)
 
-    def descuento_familia_pequena(self):
+    def test_descuento_familia_pequena(self):
         mensaje,descuento=descuentos.calcular_descuento(45, 1100, 4)
         self.assertEqual(220,descuento)
 
-    def descuento_familia_grande(self):
+    def test_descuento_familia_grande(self):
         mensaje,descuento=descuentos.calcular_descuento(45, 1100, 6)
         self.assertEqual(660,descuento)
         
-    def descuento_especial(self):
+    def test_descuento_especial(self):
         mensaje,descuento=descuentos.calcular_descuento(60, 1000, 6)
         self.assertEqual(350,descuento)
         
-    def descuento_mayores_cotizado_bajo(self):
+    def test_descuento_mayores_cotizado_bajo(self):
         mensaje,descuento=descuentos.calcular_descuento(85, 1000, 6)
         self.assertEqual(250,descuento)
 
-    def descuento_mayores_cotizado_alto(self):
+    def test_descuento_mayores_cotizado_alto(self):
         mensaje,descuento=descuentos.calcular_descuento(85, 2500, 6)
         self.assertEqual(1250,descuento)
 
-    def descuento_cliente_cotizado_bajo(self):
+    def test_descuento_cliente_cotizado_bajo(self):
         mensaje,descuento=descuentos.calcular_descuento(70, 1000, 6)
         self.assertEqual(10,descuento)
 
-    def descuento_cliente_cotizado_alto(self):
+    def test_descuento_cliente_cotizado_alto(self):
         mensaje,descuento=descuentos.calcular_descuento(70, 6000, 6)
         self.assertEqual(120,descuento)
 
